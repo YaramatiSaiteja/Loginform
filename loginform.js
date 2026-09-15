90d5a18fbd8d075370ef03e7f47d766283c9139a
YaramatiTeja <yaramatiteja@gmail.com>
2026-09-15 23:21:26 +0530

    Added validations to username and password

--- loginform.js
+++ loginform.js
@@ -1,10 +1,29 @@
 let loginForm = document.getElementById("loginForm");
 let usernameInput = document.getElementById("username");
 let passwordInput = document.getElementById("password");
+let usernameError = document.getElementById("usernameError");
+let passwordError = document.getElementById("passwordError");
+
 loginForm.addEventListener("submit",function(event){
     event.preventDefault();
     let username = usernameInput.value;
     let password = passwordInput.value;
     console.log("Username:", username);
     console.log("Password:", password);
+});
+
+usernameInput.addEventListener("blur",function(event){
+    if(event.target.value === ""){
+        usernameError.textContent = "Username is required.";
+    } else {
+        usernameError.textContent = "";
+    }
+});
+
+passwordInput.addEventListener("blur",function(event){
+    if(event.target.value === ""){
+        passwordError.textContent = "Password is required.";    
+    } else {
+        passwordError.textContent = "";
+    }
 });
