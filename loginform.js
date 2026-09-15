cb202809a60141f397ff0d47e3c0e6139b794dd5
YaramatiTeja <yaramatiteja@gmail.com>
2026-09-15 23:17:26 +0530

    Added preventing login page

--- loginform.js
+++ loginform.js
@@ -1,2 +1,10 @@
 let loginForm = document.getElementById("loginForm");
-
+let usernameInput = document.getElementById("username");
+let passwordInput = document.getElementById("password");
+loginForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    console.log("Username:", username);
+    console.log("Password:", password);
+});
