9a508aaaa6d1579d28e67572798890ac4b33dc65
YaramatiTeja <yaramatiteja@gmail.com>
2026-09-15 23:09:38 +0530

    Added Corresponding elements

--- loginform.js
+++ loginform.js
@@ -1,14 +1,2 @@
-let containerEl = document.getElementById("mainContainer");
-let headingEl = document.createElement("h1");
-headingEl.classList.add("heading");
-headingEl.textContent = "Login Form";
-containerEl.appendChild(headingEl);
+let loginForm = document.getElementById("loginForm");
 
-let labelEl = document.createElement("label");
-labelEl.setAttribute("for","username");
-labelEl.textContent = "Username: ";
-let inputEl = document.createElement("input");
-inputEl.setAttribute("type","text");
-inputEl.setAttribute("id","username");
-inputEl.setAttribute("name","username");
-containerEl.appendChild(labelEl);
